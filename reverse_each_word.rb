31d2d260d0a3b72d7781ae2365eeb8cdccd7da17
Justine Berretta <justberretta@gmail.com>
2019-05-23 21:58:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split
-    sentence_array.each do {|x| x.reverse}
+    sentence_array.each do {|x| x.reverse.join}
+      
     end 
 end 
